b2565bdd7c637460faf229f3fa5b7de2edd4bf7e
srdtrk <srdtrk@hotmail.com>
2024-04-14 18:12:16 +0700

    feat(plugins): added lualine

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+}
