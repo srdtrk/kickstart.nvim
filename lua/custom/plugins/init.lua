8697a5ff736f87ee364416a15de763e9176c3169
srdtrk <srdtrk@hotmail.com>
2024-04-14 18:37:17 +0700

    feat(plugins): added github copilot

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,8 +3,19 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  -- Lualine
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+
+  -- Github Copilot
+  {
+    'github/copilot.vim',
+    branch = 'release',
+    config = function()
+      -- Use CTRL + TAB to accept copilot suggestion
+      -- vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<CR>")', {expr = true, silent = true, noremap = true})
+    end,
   },
 }
