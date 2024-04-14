015a093134c2c1b7883ee797a3ac927a30da3db6
srdtrk <srdtrk@hotmail.com>
2024-04-14 18:55:37 +0700

    refactor(plugins): split plugins into seperate files

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,11 +1,19 @@
 return {
-  'nvim-tree/nvim-tree.lua',
-  version = '*',
-  lazy = false,
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup {
+        disable_netrw = true,
+        hijack_cursor = true,
+
+        -- set keybindings
+        vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<CR>', { desc = '[T]oggle [T]ree' }),
+      }
+    end,
   },
-  config = function()
-    require('nvim-tree').setup {}
-  end,
 }
