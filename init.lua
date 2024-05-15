01054f2b740368a641d147655f5292f99de9a6ce
srdtrk <srdtrk@hotmail.com>
2024-05-15 23:27:46 +0800

    imp: using cargo clippy for check

--- init.lua
+++ init.lua
@@ -568,7 +568,15 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {
+          settings = {
+            ['rust-analyzer'] = {
+              check = {
+                command = 'clippy',
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
