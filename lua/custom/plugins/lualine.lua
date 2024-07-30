9facf39cf1a5bbb052297b6013628ced81b961e7
srdtrk <srdtrk@hotmail.com>
2024-07-30 12:09:30 +0900

    imp: added lualine plugin

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -3,5 +3,18 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        sections = {
+          lualine_z = {
+            {
+              require('tmux-status').tmux_session,
+              cond = require('tmux-status').show,
+              padding = { left = 3 },
+            },
+          },
+        },
+      }
+    end,
   },
 }
